90f110c654b922a7356907dd2a0e6ce1f3d82826
Chin <chin.bimbo@gmail.com>
2015-06-21 14:13:17 -0400

    Make Settings class static

--- TeksavvyData/Settings.cs
+++ TeksavvyData/Settings.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 namespace TeksavvyData {
-    internal sealed class Settings {
+    internal static class Settings {
         // shitty WinRT doesn't allow public const in public class, for some bogus reasons...
         public const String ApiKey = "36A99E286BCA90747D4C6E03EA0E3C49";
         public const int DownloadLimit = 400;
